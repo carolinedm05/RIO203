1b4167047df7c220de67ad68a92256c8d71ac381
Caroline <carolinee.demulder@telecom-paris.fr>
2023-01-23 22:37:00 +0100

    envoie RSSI au thingsboard

--- rio203 position.py
+++ rio203 position.py
@@ -7,6 +7,8 @@ Created on Fri Jan 20 09:45:36 2023
 
 import serial, string
 import numpy as np
+import json
+import pycurl
 
 # Lecture des valeurs de RSSI lu par le wismote connecté au raspberry
 
@@ -26,6 +28,8 @@ res = [eval(i) for i in output_split]
 
 #valeur de RSSI de la base de données en dictionnaire pour associé des RSSI à un mettre carré de la salle 
     
+vec = np.random.uniform(-40,50,3)
+
 fingerprints_dic = {}
 fingerprints_dic[1] = [-10, -20, -30]
 fingerprints_dic[2] = [-60,-40,-1]
@@ -52,6 +56,25 @@ def closest_RSSI(vec,vec_dict):
 
 #test 
 print (closest_RSSI([0,0,0], fingerprints_dic))
+
+#fonction qui envoie d'id du mettre carré en temps réel au thingboard cloud
+
+# Préparer les données à envoyer au format JSON
+data = {"id": closest_RSSI(vec, fingerprints_dic)}
+json_data = json.dumps(data)
+
+# Initialiser la requête
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://demo.thingsboard.io/api/v1/RIO203T01/telemetry')
+c.setopt(c.HTTPHEADER, ['Content-Type: application/json'])
+c.setopt(c.POST, 1)
+c.setopt(c.POSTFIELDS, json_data)
+
+# Envoyer la requête
+c.perform()
+
+# Vérifier le code de retour
+print(c)
 
 
 
